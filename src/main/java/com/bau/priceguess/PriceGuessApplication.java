8ab0d3ad4ff467ab62583bc2f89ab61e7ecd3e9c
mertOzkan1 <muhlismertozkan99@gmail.com>
2024-03-15 13:57:12 +0300

    Initial commit

---
+++ src/main/java/com/bau/priceguess/PriceGuessApplication.java
@@ -0,0 +1,13 @@
+package com.bau.priceguess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceGuessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceGuessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bau/priceguess/PriceGuessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bau.priceguess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceGuessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
